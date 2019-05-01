06b46f41aa8fe552637685fc68de98202dad6a64
Cheyenne Hale <Cheyenne.C.Hale-1@ou.edu>
2019-05-01 11:26:12 -0500

    in progress

--- FinalExamReview/MainWindow.xaml.cs
+++ FinalExamReview/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json; //Add this first
 
 namespace FinalExamReview
 {
@@ -23,6 +25,36 @@ namespace FinalExamReview
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient()) //add using HTTPClient
+            {
+                //add resposne from webpage MUST ADD .RESULT AT END!!!!!
+                var response = client.GetAsync($@"http://pcbstuou.w27.wh-2.com/webservices/3033/api/Movies?number=100").Result;
+                //see if webpage is successful
+                if (response.IsSuccessStatusCode)
+                {
+                    //read all content from HTTP link as a string and get the result.
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    //Call the class to create a deserialized object.
+                    List<Movies> m = JsonConvert.DeserializeObject<List<Movies>>(content);
+
+                    foreach (var movie in m)
+                    {
+                        var count = 0;
+                        var total = 0;
+                        count++;
+                        total =+ count;
+
+                        lstRobertDowney.Items.Add(total);
+                    }
+                  
+                
+                    
+                }
+            }
         }
     }
 }
---
+++ FinalExamReview/Movies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamReview
+{
+    class Movies
+    {
+        public string movie_Id { get; set;}
+        public string actor_1_name { get; set; }
+        public string genre { get; set; }
+        public double imdb_score { get; set; }
+        public int num_voted_users { get; set; }
+        public string director_name { get; set; }
+
+    }
+}
